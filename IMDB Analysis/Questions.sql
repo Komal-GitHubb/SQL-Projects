242bd85e8fdb124a04094156afb0a78ae435006f
Komal Gupta <dazlerkomal@gmail.com>
2024-05-31 13:35:59 +0530

    Add files via upload

---
+++ IMDB Analysis/Questions.sql
@@ -0,0 +1,338 @@
+-- Segment 1:
+
+-- Q1. Find the total number of rows in each table of the schema?
+
+
+-- Q2. Which columns in the movie table have null values?
+
+
+-- Q3. Find the total number of movies released each year? How does the trend look month wise? (Output expected)
+
+/* Output format for the first part:
+
++---------------+-------------------+
+| Year			|	number_of_movies|
++-------------------+----------------
+|	2017		|	2134			|
+|	2018		|		.			|
+|	2019		|		.			|
++---------------+-------------------+
+
+
+Output format for the second part of the question:
++---------------+-------------------+
+|	month_num	|	number_of_movies|
++---------------+----------------
+|	1			|	 134			|
+|	2			|	 231			|
+|	.			|		.			|
++---------------+-------------------+ */
+
+
+  
+-- Q4. How many movies were produced in the USA or India in the year 2019??
+
+
+
+-- Q5. Find the unique list of the genres present in the data set?
+
+
+
+-- Q6.Which genre had the highest number of movies produced overall?
+
+
+-- Q7. How many movies belong to only one genre?
+
+
+
+-- Q8.What is the average duration of movies in each genre? 
+
+/* Output format:
+
++---------------+-------------------+
+| genre			|	avg_duration	|
++-------------------+----------------
+|	thriller	|		105			|
+|	.			|		.			|
+|	.			|		.			|
++---------------+-------------------+ */
+
+
+
+-- Q9.What is the rank of the ‘thriller’ genre of movies among all the genres in terms of number of movies produced? 
+
+
+/* Output format:
++---------------+-------------------+---------------------+
+| genre			|		movie_count	|		genre_rank    |	
++---------------+-------------------+---------------------+
+|drama			|	2312			|			2		  |
++---------------+-------------------+---------------------+*/
+
+
+
+
+
+
+
+-- Segment 2:
+
+
+-- Q10.  Find the minimum and maximum values in  each column of the ratings table except the movie_id column?
+/* Output format:
++---------------+-------------------+---------------------+----------------------+-----------------+-----------------+
+| min_avg_rating|	max_avg_rating	|	min_total_votes   |	max_total_votes 	 |min_median_rating|min_median_rating|
++---------------+-------------------+---------------------+----------------------+-----------------+-----------------+
+|		0		|			5		|	       177		  |	   2000	    		 |		0	       |	8			 |
++---------------+-------------------+---------------------+----------------------+-----------------+-----------------+*/
+
+
+-- Q11. Which are the top 10 movies based on average rating?
+/* Output format:
++---------------+-------------------+---------------------+
+| title			|		avg_rating	|		movie_rank    |
++---------------+-------------------+---------------------+
+| Fan			|		9.6			|			5	  	  |
+|	.			|		.			|			.		  |
+|	.			|		.			|			.		  |
+|	.			|		.			|			.		  |
++---------------+-------------------+---------------------+*/
+
+
+
+-- Q12. Summarise the ratings table based on the movie counts by median ratings.
+/* Output format:
+
++---------------+-------------------+
+| median_rating	|	movie_count		|
++-------------------+----------------
+|	1			|		105			|
+|	.			|		.			|
+|	.			|		.			|
++---------------+-------------------+ */
+
+
+
+-- Q13. Which production house has produced the most number of hit movies (average rating > 8)??
+/* Output format:
++------------------+-------------------+---------------------+
+|production_company|movie_count	       |	prod_company_rank|
++------------------+-------------------+---------------------+
+| The Archers	   |		1		   |			1	  	 |
++------------------+-------------------+---------------------+*/
+
+
+-- Q14. How many movies released in each genre during March 2017 in the USA had more than 1,000 votes?
+/* Output format:
+
++---------------+-------------------+
+| genre			|	movie_count		|
++-------------------+----------------
+|	thriller	|		105			|
+|	.			|		.			|
+|	.			|		.			|
++---------------+-------------------+ */
+
+
+-- Q15. Find movies of each genre that start with the word ‘The’ and which have an average rating > 8?
+/* Output format:
++---------------+-------------------+---------------------+
+| title			|		avg_rating	|		genre	      |
++---------------+-------------------+---------------------+
+| Theeran		|		8.3			|		Thriller	  |
+|	.			|		.			|			.		  |
+|	.			|		.			|			.		  |
+|	.			|		.			|			.		  |
++---------------+-------------------+---------------------+*/
+
+
+
+-- Q16. Of the movies released between 1 April 2018 and 1 April 2019, how many were given a median rating of 8?
+
+
+
+-- Q17. Do German movies get more votes than Italian movies? 
+-- Hint: Here you have to find the total number of votes for both German and Italian movies.
+
+
+
+
+
+
+
+-- Segment 3:
+
+
+-- Q18. Which columns in the names table have null values??
+/*Hint: You can find null values for individual columns or follow below output format
++---------------+-------------------+---------------------+----------------------+
+| name_nulls	|	height_nulls	|date_of_birth_nulls  |known_for_movies_nulls|
++---------------+-------------------+---------------------+----------------------+
+|		0		|			123		|	       1234		  |	   12345	    	 |
++---------------+-------------------+---------------------+----------------------+*/
+
+
+
+-- Q19. Who are the top three directors in the top three genres whose movies have an average rating > 8?
+-- (Hint: The top three genres would have the most number of movies with an average rating > 8.)
+/* Output format:
+
++---------------+-------------------+
+| director_name	|	movie_count		|
++---------------+-------------------|
+|James Mangold	|		4			|
+|	.			|		.			|
+|	.			|		.			|
++---------------+-------------------+ */
+
+
+-- Q20. Who are the top two actors whose movies have a median rating >= 8?
+/* Output format:
+
++---------------+-------------------+
+| actor_name	|	movie_count		|
++-------------------+----------------
+|Christain Bale	|		10			|
+|	.			|		.			|
++---------------+-------------------+ */
+
+
+
+-- Q21. Which are the top three production houses based on the number of votes received by their movies?
+/* Output format:
++------------------+--------------------+---------------------+
+|production_company|vote_count			|		prod_comp_rank|
++------------------+--------------------+---------------------+
+| The Archers		|		830			|		1	  		  |
+|	.				|		.			|			.		  |
+|	.				|		.			|			.		  |
++-------------------+-------------------+---------------------+*/
+
+
+
+-- Q22. Rank actors with movies released in India based on their average ratings. Which actor is at the top of the list?
+-- Note: The actor should have acted in at least five Indian movies. 
+-- (Hint: You should use the weighted average based on votes. If the ratings clash, then the total number of votes should act as the tie breaker.)
+
+/* Output format:
++---------------+-------------------+---------------------+----------------------+-----------------+
+| actor_name	|	total_votes		|	movie_count		  |	actor_avg_rating 	 |actor_rank	   |
++---------------+-------------------+---------------------+----------------------+-----------------+
+|	Yogi Babu	|			3455	|	       11		  |	   8.42	    		 |		1	       |
+|		.		|			.		|	       .		  |	   .	    		 |		.	       |
+|		.		|			.		|	       .		  |	   .	    		 |		.	       |
+|		.		|			.		|	       .		  |	   .	    		 |		.	       |
++---------------+-------------------+---------------------+----------------------+-----------------+*/
+
+
+
+-- Q23.Find out the top five actresses in Hindi movies released in India based on their average ratings? 
+-- Note: The actresses should have acted in at least three Indian movies. 
+-- (Hint: You should use the weighted average based on votes. If the ratings clash, then the total number of votes should act as the tie breaker.)
+/* Output format:
++---------------+-------------------+---------------------+----------------------+-----------------+
+| actress_name	|	total_votes		|	movie_count		  |	actress_avg_rating 	 |actress_rank	   |
++---------------+-------------------+---------------------+----------------------+-----------------+
+|	Tabu		|			3455	|	       11		  |	   8.42	    		 |		1	       |
+|		.		|			.		|	       .		  |	   .	    		 |		.	       |
+|		.		|			.		|	       .		  |	   .	    		 |		.	       |
+|		.		|			.		|	       .		  |	   .	    		 |		.	       |
++---------------+-------------------+---------------------+----------------------+-----------------+*/
+
+
+
+/* Q24. Select thriller movies as per avg rating and classify them in the following category: 
+
+			Rating > 8: Superhit movies
+			Rating between 7 and 8: Hit movies
+			Rating between 5 and 7: One-time-watch movies
+			Rating < 5: Flop movies
+--------------------------------------------------------------------------------------------*/
+
+
+
+
+
+
+-- Segment 4:
+
+-- Q25. What is the genre-wise running total and moving average of the average movie duration? 
+-- (Note: You need to show the output table in the question.) 
+/* Output format:
++---------------+-------------------+---------------------+----------------------+
+| genre			|	avg_duration	|running_total_duration|moving_avg_duration  |
++---------------+-------------------+---------------------+----------------------+
+|	comdy		|			145		|	       106.2	  |	   128.42	    	 |
+|		.		|			.		|	       .		  |	   .	    		 |
+|		.		|			.		|	       .		  |	   .	    		 |
+|		.		|			.		|	       .		  |	   .	    		 |
++---------------+-------------------+---------------------+----------------------+*/
+
+
+-- Q26. Which are the five highest-grossing movies of each year that belong to the top three genres? 
+-- (Note: The top 3 genres would have the most number of movies.)
+
+/* Output format:
++---------------+-------------------+---------------------+----------------------+-----------------+
+| genre			|	year			|	movie_name		  |worldwide_gross_income|movie_rank	   |
++---------------+-------------------+---------------------+----------------------+-----------------+
+|	comedy		|			2017	|	       indian	  |	   $103244842	     |		1	       |
+|		.		|			.		|	       .		  |	   .	    		 |		.	       |
+|		.		|			.		|	       .		  |	   .	    		 |		.	       |
+|		.		|			.		|	       .		  |	   .	    		 |		.	       |
++---------------+-------------------+---------------------+----------------------+-----------------+*/
+
+
+
+-- Q27.  Which are the top two production houses that have produced the highest number of hits (median rating >= 8) among multilingual movies?
+/* Output format:
++-------------------+-------------------+---------------------+
+|production_company |movie_count		|		prod_comp_rank|
++-------------------+-------------------+---------------------+
+| The Archers		|		830			|		1	  		  |
+|	.				|		.			|			.		  |
+|	.				|		.			|			.		  |
++-------------------+-------------------+---------------------+*/
+
+
+
+-- Q28. Who are the top 3 actresses based on number of Super Hit movies (average rating >8) in drama genre?
+/* Output format:
++---------------+-------------------+---------------------+----------------------+-----------------+
+| actress_name	|	total_votes		|	movie_count		  |actress_avg_rating	 |actress_rank	   |
++---------------+-------------------+---------------------+----------------------+-----------------+
+|	Laura Dern	|			1016	|	       1		  |	   9.60			     |		1	       |
+|		.		|			.		|	       .		  |	   .	    		 |		.	       |
+|		.		|			.		|	       .		  |	   .	    		 |		.	       |
++---------------+-------------------+---------------------+----------------------+-----------------+*
+
+
+
+/* Q29. Get the following details for top 9 directors (based on number of movies)
+Director id
+Name
+Number of movies
+Average inter movie duration in days
+Average movie ratings
+Total votes
+Min rating
+Max rating
+total movie durations
+
+Format:
++---------------+-------------------+---------------------+----------------------+--------------+--------------+------------+------------+----------------+
+| director_id	|	director_name	|	number_of_movies  |	avg_inter_movie_days |	avg_rating	| total_votes  | min_rating	| max_rating | total_duration |
++---------------+-------------------+---------------------+----------------------+--------------+--------------+------------+------------+----------------+
+|nm1777967		|	A.L. Vijay		|			5		  |	       177			 |	   5.65	    |	1754	   |	3.7		|	6.9		 |		613		  |
+|	.			|		.			|			.		  |	       .			 |	   .	    |	.		   |	.		|	.		 |		.		  |
+|	.			|		.			|			.		  |	       .			 |	   .	    |	.		   |	.		|	.		 |		.		  |
+|	.			|		.			|			.		  |	       .			 |	   .	    |	.		   |	.		|	.		 |		.		  |
+|	.			|		.			|			.		  |	       .			 |	   .	    |	.		   |	.		|	.		 |		.		  |
+|	.			|		.			|			.		  |	       .			 |	   .	    |	.		   |	.		|	.		 |		.		  |
+|	.			|		.			|			.		  |	       .			 |	   .	    |	.		   |	.		|	.		 |		.		  |
+|	.			|		.			|			.		  |	       .			 |	   .	    |	.		   |	.		|	.		 |		.		  |
+|	.			|		.			|			.		  |	       .			 |	   .	    |	.		   |	.		|	.		 |		.		  |
++---------------+-------------------+---------------------+----------------------+--------------+--------------+------------+------------+----------------+
+
+--------------------------------------------------------------------------------------------*/
+
